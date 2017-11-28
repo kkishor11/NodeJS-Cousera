e2df1c4726e2bc055332c804bdc790065547ad08
Kundan <kkishor@outlook.com>
2017-11-29 00:27:23 +0400

    Passport Authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -53,21 +55,19 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
-    var err = new Error('You are not authenticated');
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
     res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 403;
-    return next(err);
+    err.status = 401;
+    next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      next();
-    } else {
-      var err = new Error('You are not authenticated');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
