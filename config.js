7a2e6ce2a94586210b9dc340ed188b27b765cc5e
Kundan Kishor <kkishor@outlook.com>
2017-11-30 14:55:43 +0400

    Oauth icomplete code. Laptop's battery is running out

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    "secretKey": "LEARNING_IS_FUN",
-    "mongoUrl": 'mongodb://localhost:27017/confusion'
+    secretKey: 'LEARNING_IS_FUN',
+    mongoUrl: 'mongodb://localhost:27017/confusion',
+    clientId: '1751494421822205',
+    clientSecret: '86fb6e0f7a8f86c64a71126834b22aaf'
 }
